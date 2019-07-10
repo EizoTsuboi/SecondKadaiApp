9144cb459224a06068e699e1b2649195b9567fe8
EizoTsuboi <eizo.wakuwaku@gmail.com>
2019-07-10 20:36:29 +0900

    画面の遷移 ラベルの追加

---
+++ SecondKadaiApp/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 坪井衛三 on 2019/07/10.
+//  Copyright © 2019 Eizo Tsuboi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
